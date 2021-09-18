d09329162b16abd3ee5e869379c8d9ebab0bab3c
Lucas Favoreto <lfavoreto@hotmail.com>
2021-09-18 02:25:49 -0300

    add folha de estilos externa

--- appIMC/App.js
+++ appIMC/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, Image, Pressable, StyleSheet, TextInput} from 'react-native';
+import { styles } from './styles';
 
 class App extends Component{
   constructor(props){
@@ -77,49 +78,5 @@ class App extends Component{
     )
   }
 }
-
-const styles = StyleSheet.create({
-  area: {
-    alignItems: 'center',
-    marginTop: 40
-  },
-  textoPrincipal: {
-    textAlign: 'center',
-    fontSize: 22,
-    fontWeight: 'bold'
-  },
-  image: {
-    width: 120,
-    height: 120,
-    marginTop: 25,
-  },
-  input: {
-    height: 45,
-    borderWidth: 1,
-    borderColor: '#222',
-    margin: 10,
-    marginTop: 30,
-    fontSize: 20,
-    padding: 10,
-    width: 300
-  },
-  botao: {
-    backgroundColor: '#02a1dc',
-    marginTop: 30,
-  },
-  textoBotao: {
-    color: '#FFF',
-    padding: 10,
-    width: 225,
-    textAlign: 'center',
-    fontSize: 19,
-    fontWeight: 'bold',
-    width: 300
-  },
-  verificar: {
-    fontSize: 17,
-    marginTop: 30
-  }
-});
  
 export default App;
---
+++ appIMC/styles.js
@@ -0,0 +1,47 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    area: {
+      alignItems: 'center',
+      marginTop: 40
+    },
+    textoPrincipal: {
+      textAlign: 'center',
+      fontSize: 22,
+      fontWeight: 'bold'
+    },
+    image: {
+      width: 120,
+      height: 120,
+      marginTop: 25,
+    },
+    input: {
+      height: 45,
+      borderWidth: 1,
+      borderColor: '#222',
+      margin: 10,
+      marginTop: 30,
+      fontSize: 20,
+      padding: 10,
+      width: 300
+    },
+    botao: {
+      backgroundColor: '#02a1dc',
+      marginTop: 30,
+    },
+    textoBotao: {
+      color: '#FFF',
+      padding: 10,
+      width: 225,
+      textAlign: 'center',
+      fontSize: 19,
+      fontWeight: 'bold',
+      width: 300
+    },
+    verificar: {
+      fontSize: 17,
+      marginTop: 30
+    }
+  });
+
+export {styles};
